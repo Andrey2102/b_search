e2d8bb8ae9c4cb2cb08257d678f3c0e7aece1d24
Andrey2102 <andr.tutun@gmail.com>
2019-04-23 12:47:10 +0300

    Create main

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <map>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+	
+	return 0;
+}
