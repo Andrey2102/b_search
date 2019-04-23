8d93435586be8f083c4aba00ec3ee64b8c61594d
Andrey2102 <andr.tutun@gmail.com>
2019-04-23 12:57:02 +0300

    +Number generator

---
+++ untitled.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <map>
+#include <list>
+#include <algorithm>
+using namespace std;
+
+int input_size(){
+	cout << "Enter size of the array: ";
+	int SIZE;
+	cin >> SIZE;
+	return SIZE;
+}
+
+
+void output(int *A, int SIZE){
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << setw(4) << A[i];
+	}
+}
+
+	int* input(int* A, int SIZE){
+		cout << "Enter elements of thr array: ";
+	for (int i = 0; i < SIZE; ++i)
+	{
+		cin >> A[i];
+	}
+	cout << endl << endl << "Array: ";
+	output(A, SIZE);
+	return A;
+}
+
+int* generate(int* A, int SIZE){
+	srand(time(NULL));
+	cout << "Enter limit of the number that will generated: ";
+	int lim;
+	cin >> lim;
+	for (int i = 0; i < SIZE; ++i)
+	{
+		A[i] = rand()%(lim + 1);
+	}
+	cout << endl << endl << "Generated array: ";
+	output(A, SIZE);
+	return A;
+}
+
+int* generate_revert(int* A, int SIZE){
+	srand(time(NULL));
+	for (int i = 0; i < SIZE; ++i)
+	{
+		A[i] = SIZE - i;
+	}
+	cout << endl << endl << "Generated array: ";
+	output(A, SIZE);
+	return A;
+	}
+	
+	int* select(int* A, int SIZE){
+	cout << "If you want to enter numbers yourself, press (1) and press (2) to generate random numbers (press (3) to generate an array sorted in the opposite direction): ";
+	int sel = 0;
+	cin >> sel;
+	switch(sel){
+		case 1:
+			A = input(A, SIZE);
+			break;
+		case 2:
+			A = generate(A, SIZE);
+			break;
+		case 3:
+			A = generate_revert(A, SIZE);
+			break;
+		default:
+			cout << "Default select." << endl;
+			select(A, SIZE);
+	}
+	return A;
+}
+
+int main()
+{
+	
+	return 0;
+}
